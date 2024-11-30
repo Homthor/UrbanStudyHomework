f84d1b0f5a59ed17ad69eb5286474c4538a8ba64
Alex <157229322+Homthor@users.noreply.github.com>
2024-11-30 23:19:50 +0300

    Задача "Всё не так уж просто"
    Цикл for. Элементы списка. Полезные функции в цикле

---
+++ module_2_4.py
@@ -0,0 +1,35 @@
+# Задача "Всё не так уж просто":
+# Дан список чисел numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# Используя этот список составьте второй список primes содержащий только простые числа.
+# А так же третий список not_primes, содержащий все не простые числа.
+# Выведите списки primes и not_primes на экран(в консоль).
+# Пункты задачи:
+# Создайте пустые списки primes и not_primes.
+# При помощи цикла for переберите список numbers.
+# Напишите ещё один цикл for (вложенный), где будут подбираться делители для числа из 1ого цикла.
+# Отметить простоту числа можно переменной is_prime, записав в неё значение True перед проверкой.
+# В процессе проверки на простоту записывайте числа из списка numbers в списки primes и not_primes
+# в зависимости от значения переменной is_prime после проверки (True - в prime, False - в not_prime).
+# Выведите списки primes и not_primes на экран(в консоль).
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for i in range(len(numbers)+1):
+    counter = 0
+    is_prime = True
+    for j in range(1, i):
+        if i % j == 0:
+            counter += 1
+            if counter >= 2:
+                is_prime = False
+            if is_prime == True:
+                primes.append(i)
+            if is_prime == False:
+                not_primes.append(i)
+                primes.remove(i)
+                break
+
+print(primes)
+print(not_primes)
