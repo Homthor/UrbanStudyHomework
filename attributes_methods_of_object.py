1f016bf47c6613208dd464c6cad20f1aa57d5aff
Alex <157229322+Homthor@users.noreply.github.com>
2024-12-30 19:43:02 +0300

    "Атрибуты и методы объекта"
    Задача "Developer - не только разработчик":
    
    Реализуйте класс House, объекты которого будут создаваться следующим образом:
    
    House('ЖК Эльбрус', 30)
    
    Объект этого класса должен обладать следующими атрибутами:
    
    self.name - имя, self.number_of_floors - кол-во этажей
    
    Также должен обладать методом go_to(new_floor), где new_floor - номер этажа(int), на который нужно приехать.
    
    Метод go_to выводит на экран(в консоль) значения от 1 до new_floor(включительно).
    
    Если же new_floor больше чем self.number_of_floors или меньше 1, то вывести строку "Такого этажа не существует".
    
    Пункты задачи:
    
    Создайте класс House.
    Вунтри класса House определите метод __init__, в который передадите название и кол-во этажей.
    Внутри метода __init__ создайте атрибуты объекта self.name и self.number_of_floors, присвойте им переданные значения.
    Создайте метод go_to с параметром new_floor и напишите логику внутри него на основе описания задачи.
    Создайте объект класса House с произвольным названием и количеством этажей.
    Вызовите метод go_to у этого объекта с произвольным числом.
    Пример результата выполнения программы:
    Исходные данные:
    h1 = House('ЖК Горский', 18)
    h2 = House('Домик в деревне', 2)
    h1.go_to(5)
    h2.go_to(10)
    Вывод на консоль:
    1
    2
    3
    4
    5
    "Такого этажа не существует"

---
+++ attributes_methods_of_object.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.floors = number_of_floors
+
+    def go_to(self, floor_required):
+        if floor_required <= self.floors:
+            for current_floor in range(floor_required):
+                print('Этаж', current_floor + 1)
+                sleep(1)
+        else:
+            print('Такого этажа не существует для:', self.name)
+
+
+h1 = House('ЖК Эльбрус', 30)
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(5)
+h2.go_to(10)
